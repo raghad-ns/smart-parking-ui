d56b1586107ec3ec6b482204b1eaea6ce55e0256
sajeda swad <201047@ppu.edu.ps>
2023-11-13 22:13:27 +0200

    SP-11-start with pages

---
+++ src/Pages/Introduction/intro.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './intro.scss'
+import BackgroundImage from './pg.png';
+const Intro = () => {
+  return (
+    <div>
+    <div className="intro">introoo</div>,
+    </div>
+
+  )
+};
+
+export default Intro;
