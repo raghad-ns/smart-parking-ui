b2df31de394d4319f91a937f7250eafc6af51192
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-12-03 11:11:58 +0200

    Add pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Intro from './Pages/Introduction/intro';
 import Login from './Pages/Login/login';
 import logo from "./ps-logo.png";
 import Register from './Pages/Registration/register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className="background-image">
-      <img className="logo" src={logo} alt="Logo" />
-      {/* <Intro/> */}
-      {/* <Login/> */}
-      <Register/>
-
+        <img className="logo" src={logo} alt="Logo" />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Intro />} />
+            <Route path='/signin' element={<Login />} />
+            <Route path='/signup' element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      </div>
+    </div>
   );
 }
 
