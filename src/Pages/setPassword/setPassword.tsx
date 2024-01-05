77e4a6fba9f89bd632e44e991fed20cd4f42c79d
raghad-ns <raghadnadersalem20022017@gmail.com>
2024-01-05 22:36:53 +0200

    reset password enhancements

--- src/Pages/setPassword/setPassword.tsx
+++ src/Pages/setPassword/setPassword.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import "./setPassword.scss";
 import { InputAdornment } from "@mui/material";
-import { Key } from "@mui/icons-material";
+import { RemoveRedEye, VisibilityOff } from "@mui/icons-material";
 import { passwordSetService } from "../../services/user.service";
 import { useNavigate } from "react-router-dom";
 import { validateInputs } from "../../utils/utils";
 
 const SetPassword = () => {
   const [password, setPassword] = useState<string>("");
+  const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false)
+  const [confirmPasswordVisibility, setConfirmPasswordVisibility] = useState<boolean>(false)
   const navigate = useNavigate()
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const handlePasswordSetting = async () => {
@@ -16,12 +18,16 @@ const SetPassword = () => {
       { value: confirmPassword, type: "password" }
     ]
     if (validateInputs(inputs)) {
-      const setPassword = await passwordSetService(password, confirmPassword)
-      if (setPassword.state) {
-        window.alert('password set successfully!')
-        navigate('/info', { replace: true })
+      if (password === confirmPassword) {
+        const setPassword = await passwordSetService(password, confirmPassword)
+        if (setPassword.state) {
+          window.alert('password set successfully!')
+          navigate('/info', { replace: true })
+        } else {
+          window.alert('invalid password!')
+        }
       } else {
-        window.alert('invalid password!')
+        window.alert('Password confirmation does not match the password you entered!')
       }
     } else {
       window.alert('Password must be at least 8 characters containing digits, special symbols, upper and lower case letters')
@@ -32,11 +38,13 @@ const SetPassword = () => {
       <div className="input-group">
         <span>
           <InputAdornment position="start">
-            <Key className="icons" />
+            <button className="invisible" onClick={() => setPasswordVisibility(!passwordVisibility)}>
+              {passwordVisibility ? <VisibilityOff className="icons" /> : <RemoveRedEye className="icons" />}
+            </button>
           </InputAdornment>
         </span>
         <input
-          type="password"
+          type={passwordVisibility ? "text" : "password"}
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -46,11 +54,13 @@ const SetPassword = () => {
       <div className="input-group">
         <span>
           <InputAdornment position="start">
-            <Key className="icons" />
+            <button className="invisible" onClick={() => setConfirmPasswordVisibility(!confirmPasswordVisibility)}>
+              {confirmPasswordVisibility ? <VisibilityOff className="icons" /> : <RemoveRedEye className="icons" />}
+            </button>
           </InputAdornment>
         </span>
         <input
-          type="password"
+          type={confirmPasswordVisibility ? "text" : "password"}
           id="confirmPassword"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
