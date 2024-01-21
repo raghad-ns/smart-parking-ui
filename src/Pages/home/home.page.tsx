e6638e2a81ed898102ed6d15a59fc2d1a32c164c
raghad-ns <raghadnadersalem20022017@gmail.com>
2024-01-21 11:36:39 +0200

    user & manager views

--- src/Pages/home/home.page.tsx
+++ src/Pages/home/home.page.tsx
@@ -1,10 +1,59 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './home.scss'
+import { UserContext } from '../../providers/user.provider'
+import React from 'react'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const user = React.useContext(UserContext);
+  console.log(user.user?.role)
   return (
     <div className='homeWrapper'>
-      <Link to='/signup'>Add new car</Link>
+      {(user.user?.role.roleName === 'Manager' || user.user?.role.roleName === 'Admin') &&
+        <div className="user-buttons-wrapper">
+          <h1>Wellcome manager</h1>
+          <div className="button2" onClick={() => navigate('/signup')}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Enroll new car
+          </div>
+          <div className="button2" onClick={() => navigate('/parking-enrollment')}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Enroll new parking
+          </div>
+        </div>
+      }
+      {(user.user?.role.roleName === 'User') &&
+        <div className="user-buttons-wrapper">
+          <h1>Wellcome Car owner</h1>
+          <div className="button2" onClick={() => navigate('/parking')}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Use parking
+          </div>
+          <div className="button2" onClick={() => navigate('/charge-wallet')}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Charge car's wallet
+          </div>
+          <div className="button2" onClick={() => navigate('/history')}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            View car's parking history
+          </div>
+        </div>
+      }
     </div>
   )
 }
